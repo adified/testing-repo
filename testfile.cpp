ae186ddb002a12d7ee7300c689bdcddd54de3ebd
adified <105724242+adified@users.noreply.github.com>
2022-12-20 12:53:08 +0530

    Update testfile.cpp

--- testfile.cpp
+++ testfile.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 #include <array>
 #include <math.h>
+// i wish to show this in demo
 //this is not and edit this is only to test my api
 using namespace std;
 double cube(double num);        //function prototype
